11fc91641d035d3798b54739f3783001d5441173
Vijay Kumar <Be1newinner@gmail.com>
2025-05-20 15:12:57 +0530

    feat: add text animation effects and new TextEffects component

---
+++ motion_animations/src/app/text/page.tsx
@@ -0,0 +1,121 @@
+import React from "react";
+
+export default function TextEffects() {
+  return (
+    <div className="flex flex-col min-h-screen bg-red-50">
+      <div className="flex justify-center items-center flex-1 text-4xl font-bold text-gray-800 *:inline-flex *:flex-col text-container relative">
+        <div>
+          <span>W</span>
+          <span>W</span>
+        </div>
+        <div>
+          <span>E</span>
+          <span>E</span>
+        </div>
+        <div>
+          <span>L</span>
+          <span>L</span>
+        </div>
+        <div>
+          <span>C</span>
+          <span>C</span>
+        </div>
+        <div>
+          <span>O</span>
+          <span>O</span>
+        </div>
+        <div>
+          <span>M</span>
+          <span>M</span>
+        </div>
+        <div>
+          <span>E</span>
+          <span>E</span>
+        </div>
+      </div>
+
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+    </div>
+  );
+}
